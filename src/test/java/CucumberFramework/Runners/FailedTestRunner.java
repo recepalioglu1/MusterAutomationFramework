da79851cb8f41c104eafdbb0e93d6386ac4a1560
recepalioglu1 <recepalioglu1@gmail.com>
2020-10-04 16:00:49 +0200

    first commit

---
+++ src/test/java/CucumberFramework/Runners/FailedTestRunner.java
@@ -0,0 +1,1 @@
+package CucumberFramework.Runners;import io.cucumber.junit.Cucumber;import io.cucumber.junit.CucumberOptions;import org.junit.runner.RunWith;@RunWith(Cucumber.class)@CucumberOptions(        plugin = {"html:target/failed-html-report"},        features = "@target/rerun.txt",        glue = "com/VyTrack/Step_Definitions")public class FailedTestRunner {}
